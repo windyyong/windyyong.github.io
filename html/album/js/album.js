cac458bf02e8650048de4763c846310a1dfb9604
windy <530650681@qq.com>
2017-10-20 21:21:21 +0800

    Site updated: 2017-10-20 21:21:20

--- html/album/js/album.js
+++ html/album/js/album.js
@@ -18,4 +18,19 @@ window.onload=function(){
             oBox.style.width="80%";
             oBox.style.transform="rotateY(720deg)";
 
-    		for(var i=0;i
+    		for(var i=0;i<aLi.length;i++){
+    			aLi[i].onclick=function(){
+    				oBigImg.src=this.getElementsByTagName("img")[0].src;
+    				oBig.style.width="60%";
+    				oBig.style.opacity=1;
+    				oBig.style.transform="rotateX(360deg)";
+    			}
+    		}
+
+    		oBtn.onclick=function(){
+    			oBig.style.width=0;
+    			oBig.style.opacity=0;
+    			oBig.style.transform="rotateX(-360deg)";
+    		}
+
+    	}
